f724bf4827c5ea19865cbfb0ec13fc3b045988d5
Maarulove <qudayev62@mail.ru>
2024-04-06 14:55:05 +0200

    accounts edited unnecesary things

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,45 +1,26 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from farmersapp.models import Users
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    phone = forms.CharField(max_length=20, required=False)
-    avatar = forms.ImageField(required=False)
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2', 'phone', 'avatar',)
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if commit:
-            user.save()
-        user_profile = Users.objects.create(user=user, phone=self.cleaned_data['phone'], avatar=self.cleaned_data['avatar'])
-        return user
+        fields = ['username', 'email', 'password1', 'password2']
 
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
 
-
-
-class UserForm(forms.ModelForm):
-    def clean_username(self):
-        """Reject usernames that differ only in case."""
-        username = self.cleaned_data.get("username")
-        if (
-            username
-            and self._meta.model.objects.filter(username__iexact=username).exists()
-        ):
-            self._update_errors(
-                ValidationError(
-                    {
-                        "username": self.instance.unique_error_message(
-                            self._meta.model, ["username"]
-                        )
-                    }
-                )
-            )
-        else:
-            return username
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
